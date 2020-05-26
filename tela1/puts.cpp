6bc0f2f5394691ed18e6625bbd9afa1f79004b78
thisLucio <luxusnietzsche@gmail.com>
2020-05-26 11:35:57 -0300

    Deixando tudo pronto

--- tela1/puts.cpp
+++ tela1/puts.cpp
@@ -3,7 +3,7 @@
 
 int main(int argc, char* argv[]) {
     SDL_Init(SDL_INIT_EVERYTHING);
-    SDL_Window* window = SDL_CreateWindow("Teste SDL",
+    SDL_Window* window = SDL_CreateWindow("SDL2",
         SDL_WINDOWPOS_UNDEFINED,
         SDL_WINDOWPOS_UNDEFINED,
         640,
